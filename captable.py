369d91d080a5c9645194514cc171ebe77ff99b25
Vincent Whales <vincent@whales.media>
2019-07-12 23:54:25 -0700

    .

--- captable.py
+++ captable.py
@@ -6,9 +6,8 @@ from datetime import datetime
 DT_FORMAT = '%Y-%m-%d'
 
 
-class Entry(object):
-    """A container that holds one CSV row. Each CSV row represents one
-    investment transaction by one investor.
+class Transaction(object):
+    """Represents one transaction by one investor into this company.
 
     Attributes:
         dt (datetime): date field of CSV row
@@ -16,27 +15,46 @@ class Entry(object):
         cash_paid (float): how much this investor has invested
         investor (str): name of the investor for this transaction
     """
-    def __init__(self, dt, row):
-        """Instantiates the entry class with a CSV row that represents an
-        investment transaction.
+    def __init__(self, row):
+        """Instantiates a Transaction instance.
 
         Args:
-            row (list): List of strings for the transaction.
-            dt (datetime): datetime for the transaction.
+            row (list): List of values for the transaction.
         """
-        self.dt = dt
+        self.dt = datetime.strptime(row[0], DT_FORMAT)
         self.shares = int(row[1])
         self.cash_paid = float(row[2])
         self.investor = row[3]
 
+
+class Investor(object):
+    """A container to hold all transaction for one investor
+
+    Attributes:
+        name (str): name of the investor
+        shares (int): total number of shares across all transactions
+        cash_paid (float): total amt of cash paid by investor across all
+            transactions.
+    """
+    def __init__(self, name):
+        self.name = name
+        self.shares = 0
+        self.cash_paid = 0
+
+    def add_transaction(self, transaction):
+        """Adds a transaction to this investor.
+        """
+        self.shares += transaction.shares
+        self.cash_paid += transaction.cash_paid
+
     def to_ownership(self, total_shares):
-        """Maps the values of this class to a dictionary object.
+        """Maps the values of this instance to a dictionary object.
 
         Args:
             total_shares (int): Total no. of outstanding shares for a co.
         """
         return dict(
-            investor=self.investor,
+            investor=self.name,
             shares=self.shares,
             cash_paid=self.cash_paid,
             ownership=round(float(self.shares) / total_shares * 100, 2)
@@ -70,8 +88,8 @@ class CapTableParser(object):
             self.dt = datetime.now()
 
         cash_raised = 0
-        total_number_of_shares = 0
-        entries = {}        # 'investor': entry
+        total_shares = 0
+        investors = {}        # 'investor': investor
 
         with open(csv_path, mode='r') as csv_file:
             csv_reader = csv.reader(csv_file)
@@ -81,28 +99,29 @@ class CapTableParser(object):
                 if not row:             # skip empty lines
                     continue
 
-                dt = datetime.strptime(row[0], DT_FORMAT)
-                if dt > self.dt:        # skip entires that happens after dt
+                transaction = Transaction(row)
+                if transaction.dt > self.dt:    # skip entires that happens after dt
                     continue
 
                 # Indiscriminate: all transactions are kosher after this point
 
-                investor = row[3]
-                if investor in entries:
-                    entry = entries[investor]
+                name = transaction.investor
+                if name in investors:
+                    investor = investors[name]
                 else:
-                    entry = Entry(dt, row)
-                    entries[investor] = entry
+                    investor = Investor(name)
+                    investors[name] = investor
 
-                cash_raised += entry.cash_paid
-                total_number_of_shares += entry.shares
+                investor.add_transaction(transaction)
+                cash_raised += investor.cash_paid
+                total_shares += investor.shares
 
         print(json.dumps(dict(
             date=self.dt.strftime(DT_FORMAT),
             cash_raised=cash_raised,
-            total_number_of_shares=total_number_of_shares,
+            total_shares=total_shares,
             ownership=[
-                e.to_ownership(total_number_of_shares) for e in entries.values()
+                i.to_ownership(total_shares) for i in investors.values()
             ],
         )))
 
